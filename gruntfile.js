91c413168bbf696330ab7e29b74039c4b8df9fb3
Cyril Ajieh <cyril.ajieh@gmail.com>
2016-08-01 21:15:10 -0500

    modified grunt config

--- gruntfile.js
+++ gruntfile.js
@@ -12,30 +12,30 @@ require('es6-promise').polyfill();
       }
     },
 //	  		
-	concat: {
-		  options: {
-			// define a string to put between each file in the concatenated output
-			separator: '\n'
-		  },
-		  dist: {
-			// the files to concatenate
-			src: 'src/js/*.js',
-			// the location of the resulting JS file
-			dest: 'dist/<%= pkg.name %>.js'
-				
-		  }
-		},
-	  uglify: {
-		  options: {
-			// the banner is inserted at the top of the output
-			banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-		  },
-		  dist: {
-			files: {
-			  'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-					}
-		  		}
+		concat: {
+			  options: {
+				// define a string to put between each file in the concatenated output
+				separator: '\n'
+			  },
+			  dist: {
+				// the files to concatenate
+				src: 'src/js/*.js',
+				// the location of the resulting JS file
+				dest: 'dist/<%= pkg.name %>.js'
+
+			  }
 			},
+		  uglify: {
+			  options: {
+				// the banner is inserted at the top of the output
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			  },
+			  dist: {
+				files: {
+				  'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+						}
+					}
+				},
 		    uncss: {
 			   dist: {
 					  options: {
@@ -43,7 +43,7 @@ require('es6-promise').polyfill();
 						 ignoreSheets: [/fonts.googleapis/],
 					  },
 					  files: {
-						 'dist/css/unused-removed.css': ['src/index2.html']
+						 'dist/css/unused-removed.css': ['src/*.html','src/templates/*.html']
 					  }
 				   }
 			},
@@ -57,33 +57,33 @@ require('es6-promise').polyfill();
 				  }
 			  }
 			},
-	  	htmlmin: {
-			   dist: {
-				  options: {
-					 removeComments: true,
-					 collapseWhitespace: true
-				  },
-				  files: [{
-					 expand: true,
-					 cwd: 'src',
-					 src: '*.html',
-					 dest: 'dist/'
-				  }]
-			   }
-			},
-	  imagemin: {
-		   dist: {
-			  options: {
-				optimizationLevel: 7
-			  },
-			  files: [{
-				 expand: true,
-				 cwd: 'src/images',
-				 src: ['*.{png,jpg,gif}'],
-				 dest: 'dist/images'
-			  }]
-		   }
-		},
+//	  	htmlmin: {
+//			   dist: {
+//				  options: {
+//					 removeComments: true,
+//					 collapseWhitespace: true
+//				  },
+//				  files: [{
+//					 expand: true,
+//					 cwd: 'src',
+//					 src: '*.html',
+//					 dest: 'dist/'
+//				  }]
+//			   }
+//			},
+//	  imagemin: {
+//		   dist: {
+//			  options: {
+//				optimizationLevel: 7
+//			  },
+//			  files: [{
+//				 expand: true,
+//				 cwd: 'src/images',
+//				 src: ['*.{png,jpg,gif}'],
+//				 dest: 'dist/images'
+//			  }]
+//		   }
+//		},
 		watch: {
 			options:{
 			livereload:true
@@ -99,11 +99,11 @@ require('es6-promise').polyfill();
   	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-uncss');
-	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
+//	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	//grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 	
 
-  grunt.registerTask('serve', ['jshint','concat','uglify','cssmin','uncss','htmlmin','imagemin','watch']);
+  grunt.registerTask('serve', ['jshint','concat','uglify','cssmin','uncss','watch']);
 
 };
